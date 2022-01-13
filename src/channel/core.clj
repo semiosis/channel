d3dfd04eee4e4c2a9493ed6c0f343920b3f9bb57
Shane Mulligan <mullikine@gmail.com>
2022-01-13 13:14:38 +1300

    Thu 13 Jan 2022 13:14:38

--- src/channel/core.clj
+++ src/channel/core.clj
@@ -1,6 +1,11 @@
 (ns channel.core
   (:gen-class))
 
+;; Why do I want to rewrite this in clojure?
+;;   $HOME/source/git/semiosis/pen.el/scripts/channel
+;;   $EMACSD/pen.el/src/pen-channel.el
+;; Because I want 
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
