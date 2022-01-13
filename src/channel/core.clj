0dc4bd66f708cf351762ed02a33239e2c90df0ea
Shane Mulligan <mullikine@gmail.com>
2022-01-13 13:17:09 +1300

    Thu 13 Jan 2022 13:17:09

--- src/channel/core.clj
+++ src/channel/core.clj
@@ -6,7 +6,8 @@
 ;;   $EMACSD/pen.el/src/pen-channel.el
 ;; Because I want to use external prompt functions (i.e. the bash interop)
 ;; and I want to have a program loop which is independent of emacs.
-;; This will be the main program loop. It may work directly with tmux.
+;; This will be the main program loop. It may work directly with tmux,
+;; to watch a terminal and send input at its own disgression.
 
 (defn -main
   "I don't do a whole lot ... yet."
